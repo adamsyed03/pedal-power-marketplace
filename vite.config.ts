41fa9bd977774217d173e0935551e99f68f13121
Adam <adamksyed03@gmail.com>
2024-12-29 02:21:30 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "127.0.0.1",
-    port: 8080,
+    port: 3000,
   },
   plugins: [
     react(),
